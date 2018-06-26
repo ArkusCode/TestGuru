f13979e64d0f02d8a482882c26df00a2eab53dc4
Arkus <arkuscode@gmail.com>
2018-06-27 02:53:16 +0300

    Lesson 2 first commit

---
+++ db/migrate/20180626231550_add_question_null_constraint.rb
@@ -0,0 +1,6 @@
+class AddQuestionNullConstraint < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null(:questions, :body, false)
+    change_column_null(:questions, :test_id, false)    
+  end
+end
