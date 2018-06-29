f95c1f2f7c18b8913dc8d580a88770aea4b75cba
Arkus <arkuscode@gmail.com>
2018-06-30 00:48:55 +0300

    Lesson3 First commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def test_by_level(level)
+    Test.joins('JOIN user_tests ON tests.id = user_tests.test_id')
+        .where(level: level, user_tests: { user_id: id })
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create([{ title: 'Frontend' },
+                 { title: 'Backend' },
+                 { title: 'Mobile Development' }])
+
+Test.create([{ title: 'Ruby', level: 0,
+               category_id: 2 },
+             { title: 'HTML', level: 2,
+               category_id: 1 },
+             { title: 'Swift', level: 1,
+               category_id: 3 }])
+
+Question.create([{ body: 'Ruby question 1',
+                   test_id: 1 },
+                 { body: 'HTML question 2',
+                   test_id: 2 },
+                 { body: 'Swift question 1',
+                   test_id: 3 }])
+
+Answer.create([{ body: 'Wrong answer 1',
+                 question_id: 1 },
+               { body: 'Wrong answer 2',
+                 question_id: 1 },
+               { body: 'Correct answer',
+                 question_id: 1, correct: true }])
+
+User.create([{ email: 'foo1@bar.com',
+               first_name: 'John',
+               last_name: 'Doe'},
+             { email: 'foo2@bar.com',
+               first_name: 'Jane',
+               last_name: 'Doe' }])
+
+UserTest.create([{ user_id: 1, test_id: 1 },
+                 { user_id: 1, test_id: 2 },
+                 { user_id: 1, test_id: 3 }])
