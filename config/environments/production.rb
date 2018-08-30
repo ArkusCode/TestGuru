b0873094f4f961313a54adfbba570dc1fcd24f4c
ArkusCode <arkuscode@gmail.com>
2018-08-30 15:08:43 +0300

    minor fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     adress:               'smtp.gmail.com',
-    port:                 587,
+    port:                 '587',
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
