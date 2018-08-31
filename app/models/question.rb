079786680a49ebb994349f11c537a032ffb48f2d
ArkusCode <arkuscode@gmail.com>
2018-08-31 22:14:09 +0300

    dependent fix

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :test
-  has_many :answers
-  has_many :gists
+  has_many :answers, dependent: :delete_all
+  has_many :gists, dependent: :delete_all
 
   validates :body, presence: true
 end
