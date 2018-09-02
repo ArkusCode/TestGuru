fb4b5abb8ecf90a1f46dfcdecaa407aee65bbbd4
ArkusCode <arkuscode@gmail.com>
2018-09-02 11:02:17 +0300

    validation fix

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -7,7 +7,7 @@ class FeedbacksController < AuthenticatedController
   def create
     @feedback = current_user.feedbacks.new(feedback_params)
 
-    if @feedback.save
+    if @feedback.valid?
       FeedbacksMailer.send_feedback(@feedback).deliver_now
       redirect_to tests_path, notice: t('.feedback_notice')
     else
